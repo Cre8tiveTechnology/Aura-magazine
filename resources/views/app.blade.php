7a1175e7075f0fdcc545aa1f58d0803cf1f05c88
ishoshot <ishoshot@gmail.com>
2020-12-05 16:44:32 +0100

    Meta Color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#000000">
     <title>Aura - Fashion, Beauty, Latest News, Trends, Arts &amp; Culture</title>
     <meta name="description"
         content="Aura brings you fashion, beauty, lifestyle, arts &amp; culture from local and foreign social scenes, culture, traditions and high-societal etiquettes." />
